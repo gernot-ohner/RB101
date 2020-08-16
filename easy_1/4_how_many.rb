6e58855850ebc473d9e175f1a78cf2711c123e18
Gernot Ohner <gernot.ohner@gmail.com>
2020-08-16 16:46:44 +0200

    [easy1] problem 4: how many, further exploration

--- easy_1/4_how_many.rb
+++ easy_1/4_how_many.rb
@@ -28,7 +28,7 @@
 def tally(list)
   counts = {}
   list.each do |element|
-    if counts.include? element
+    if counts.key? element
       counts[element] = counts[element] + 1
     else
       counts[element] = 1
@@ -51,3 +51,19 @@ p tally([]) == {}
 p tally([1, 2, 1, 1, 3]) == {1 => 3, 2 => 1, 3 => 1}
 p tally(%w[car car truck car SUV truck motorcycle motorcycle car truck]) == {"car" => 4, "truck" => 3, "SUV" => 1, "motorcycle" => 2}
 p tally(["car", 123, :hey, [1, 2, 3], [1, 2, 3], :hey]) == {"car" => 1, 123 => 1, :hey => 2, [1, 2, 3] => 2}
+
+# Further Exploration
+def case_insensitive_tally(list)
+  # this only makes sense for strings
+  # return unless list.all? { |element| element.is_a? String  }
+
+  counts = {}
+  list.each do |element|
+    counts.update({element.downcase => 1}) { |_, v1, v2| v1 + v2}
+  end
+  counts
+end
+
+p case_insensitive_tally([]) == {}
+p case_insensitive_tally(%w[car car truck car SUV truck motorcycle motorcycle car truck]) == {"car" => 4, "truck" => 3, "suv" => 1, "motorcycle" => 2}
+p case_insensitive_tally(%w[car car suv truck CAR SUV truck motorcycle motorcycle car truck]) == {"car" => 4, "truck" => 3, "suv" => 2, "motorcycle" => 2}
