d2cacee626ad7675b5af4f84186b41d6b11e730c
Gernot Ohner <gernot.ohner@gmail.com>
2020-08-16 13:14:44 +0200

    [easy1] problem 1, improved

--- easy_1/1_repeat_yourself.rb
+++ easy_1/1_repeat_yourself.rb
@@ -9,6 +9,8 @@
 # Other questions:
 # - Q: Should the method return a newline separated string or print to stdout?
 #   A: return a newline separated string. Don't have side effect's willy-nilly in your program.
+# - Q: should there be a newline after the last line?
+#   A: no
 #
 # Pseudo Code:
 # for each number from 1 to n
@@ -30,6 +32,11 @@ def repeat(string, n)
   result
 end
 
+def better_repeat(string, n)
+  return if n <= 0
+  (1..n).reduce("") { |accumulator, _| accumulator + "#{string}\n" }.chomp
+end
+
 puts repeat("what's up? ", 3)
 
 # Test cases:
@@ -38,3 +45,11 @@ p repeat("hi", 1) == "hi"
 p repeat("hi", -10) == nil
 p repeat("hi", 3) == "hi\nhi\nhi"
 p repeat("what's up? ", 3) == "what's up? \nwhat's up? \nwhat's up? "
+
+
+p better_repeat("hi", 0) == nil
+p better_repeat("hi", 1) == "hi"
+p better_repeat("hi", -10) == nil
+p better_repeat("hi", 3) == "hi\nhi\nhi"
+p better_repeat("what's up? ", 3) == "what's up? \nwhat's up? \nwhat's up? "
+
