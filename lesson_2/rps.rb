52389152e9d480ee3040ec414a67b729c88c567e
Gernot Ohner <gernot.ohner@gmail.com>
2020-08-23 14:35:38 +0200

    [lesson2] adds rps bonus feature 2

--- lesson_2/rps.rb
+++ lesson_2/rps.rb
@@ -1,9 +1,39 @@
 # frozen_string_literal: true
 
-VALID_CHOICES = %w[rock paper scissors lizard spock].freeze
+VALID_CHOICES = %i[rock paper scissors lizard spock].freeze
+
+X_BEATS_Y = {
+  rock: %i[scissors lizard],
+  paper: %i[rock spock],
+  scissors: %i[paper lizard],
+  lizard: %i[spock paper],
+  spock: %i[rock scissors]
+}.freeze
+
+def construct_prefixes(symbol)
+  (0...symbol.to_s.length).map { |i| symbol.to_s[0..i] }
+end
+
+def disambiguate_prefixes(prefixes)
+  tallies = prefixes.values.flatten.tally
+  duplicates = tallies.select { |_, v| v > 1 }.keys
+  prefixes.transform_values { |v| v.difference(duplicates) }
+end
+
+def unambiguous_prefixes(symbols)
+  prefixes = symbols.to_h { |choice| [choice, construct_prefixes(choice)] }
+  disambiguate_prefixes(prefixes).freeze
+end
+
+PREFIXES = unambiguous_prefixes(VALID_CHOICES)
+
 
 def prompt(message)
   puts "=> #{message}"
+end
+
+def prefix_to_word(prefix)
+  PREFIXES.find { |_, v| v.include? prefix }&.first || :invalid
 end
 
 def valid_choice?(string)
@@ -11,11 +41,7 @@ def valid_choice?(string)
 end
 
 def win?(player, computer)
-  (player == 'rock' && (computer == 'scissors' || computer == 'lizard')) ||
-    (player == 'paper' && (computer == 'rock' || computer == 'spock')) ||
-    (player == 'scissors' && (computer == 'paper' || computer == 'lizard')) ||
-    (player == 'spock' && (computer == 'rock' || computer == 'scissors')) ||
-    (player == 'lizard' && (computer == 'spock' || computer == 'paper'))
+  X_BEATS_Y[player.to_sym].include? computer.to_sym
 end
 
 def display_results(player, computer)
@@ -28,19 +54,21 @@ def display_results(player, computer)
   end
 end
 
-loop do
-  choice = ''
+def retrieve_valid_choice
   loop do
     prompt("Choose one #{VALID_CHOICES.join(', ')}")
-    choice = gets.chomp
+    choice = prefix_to_word(gets.chomp)
 
-    break if valid_choice? choice
+    return choice if valid_choice? choice
 
     prompt("That's not a valid choice")
   end
+end
+
+loop do
+  choice = retrieve_valid_choice
 
   computer_choice = VALID_CHOICES.sample
-  p computer_choice
 
   prompt "You chose #{choice}, computer chose #{computer_choice}"
 
